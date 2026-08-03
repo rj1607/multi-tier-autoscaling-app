7b64ad9b54088d88ebb6b9da483a427c59035d50
swapneshhrana <srhrana0405@gmail.com>
2026-08-03 15:41:43 +0530

    Repository structure prepared for AWS deployment

---
+++ database/README.md
@@ -0,0 +1,7 @@
+Databasec Files 
+
+schema.sql
+Create all database tables.
+
+seed.sql
+Adds sample data.
--- database/schema.sql
+++ database/schema.sql
@@ -7,10 +7,3 @@ CREATE TABLE IF NOT EXISTS products (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO products (name, description, price, stock)
-VALUES
-('Laptop', 'High Performance Laptop', 50000.00, 15),
-('Keyboard', 'Mechanical Keyboard', 2500.00, 30),
-('Mouse', 'Wireless Mouse', 1200.00, 50),
-('Monitor', '24 Inch LED Monitor', 12000.00, 10),
-('Headphones', 'Noise Cancelling Headphones', 4500.00, 25);
